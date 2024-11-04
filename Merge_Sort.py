c5a8e8d5cdb2e7d5e1d86c4195de3d0287e891a7
linuxmonster <69184961+linuxmnster@users.noreply.github.com>
2024-11-04 18:26:13 +0530

    Add files via upload

---
+++ Merge_Sort.py
@@ -0,0 +1,32 @@
+def mergesort(array): 
+    if(len(array)>1): 
+        mid=len(array)//2 
+        leftlist=array[:mid] 
+        rightlist=array[mid:] 
+        mergesort(leftlist) 
+        mergesort(rightlist) 
+        i=0 
+        j=0 
+        k=0 
+        while(i<len(leftlist) and j<len(rightlist)): 
+            if(leftlist[i]<rightlist[j]): 
+                array[k]=leftlist[i] 
+                i=i+1 
+                k=k+1 
+            else: 
+                array[k]=rightlist[j] 
+                j=j+1 
+                k=k+1 
+        while(i<len(leftlist)): 
+            array[k]=leftlist[i] 
+            i=i+1 
+            k=k+1 
+        while(j<len(rightlist)): 
+            array[k]=rightlist[j] 
+            j=j+1 
+            k=k+1 
+
+n=int(input("How many numbers in array: ")) 
+array=[int(input())for x in range(n)] 
+mergesort(array) 
+print("After sorting array is:",array)
